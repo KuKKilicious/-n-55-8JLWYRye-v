525f1b84beb3eb3ef92062592d958daab2d9c503
KuKKilicious <clouseyda@yahoo.de>
2016-06-03 11:27:08 +0200

    Create ListSolution.py

---
+++ ListSolution.py
@@ -0,0 +1,20 @@
+'''
+Created on May 29, 2016
+
+@author: KuKKi
+'''
+from Solution import Solution
+class ListSolution(Solution):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    listsol=None
+    def setit(self, listsol):
+        self.listsol=listsol
+    def getit(self):
+        return self.index
