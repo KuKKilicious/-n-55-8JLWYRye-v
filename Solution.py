67aa967975cd4db49cd6b51d4909e6fabeea99eb
KuKKilicious <clouseyda@yahoo.de>
2016-06-03 11:26:45 +0200

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,24 @@
+'''
+Created on May 29, 2016
+
+@author: KuKKi
+'''
+import abc
+class Solution(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    
+    @abc.abstractmethod
+    def getit(self):
+        return
+    @abc.abstractmethod
+    def setit(self):
+        return
+        
